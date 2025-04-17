ae4edde831384ac69345cd3e7234a6fa0c100ac4
abcna <loki.cna@gmail.com>
2025-04-17 01:31:45 +0000

    Fix build2 configuration for Vercel deployment

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
-import "../theme/pages.css";
-import { IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonFabList,
+  IonIcon,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+} from "@ionic/react";
 import {
   add,
   chevronUpCircle,
   colorPalette,
   document,
   globe,
-} from 'ionicons/icons';
-import React from 'react';
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+} from "ionicons/icons";
+import "../theme/pages.css";
+
 const LibraryPage = () => (
   <>
     <IonHeader>
@@ -19,45 +32,47 @@ const LibraryPage = () => (
       </IonToolbar>
     </IonHeader>
     <IonContent>
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-
-      <IonButton fill="clear">Action 1</IonButton>
-      <IonButton fill="clear">Action 2</IonButton>
-    </IonCard>
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-
-      <IonButton fill="clear">Action 1</IonButton>
-      <IonButton fill="clear">Action 2</IonButton>
-    </IonCard>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+          Here's a small text description for the card content. Nothing more,
+          nothing less.
+        </IonCardContent>
+        <IonButton fill="clear">Action 1</IonButton>
+        <IonButton fill="clear">Action 2</IonButton>
+      </IonCard>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+          Here's a small text description for the card content. Nothing more,
+          nothing less.
+        </IonCardContent>
+        <IonButton fill="clear">Action 1</IonButton>
+        <IonButton fill="clear">Action 2</IonButton>
+      </IonCard>
       <div className="centered-content">Library content</div>
       <IonFab slot="fixed" vertical="bottom" horizontal="end">
+        <IonFabButton>
+          <IonIcon icon={add}></IonIcon>
+        </IonFabButton>
+        <IonFabList side="top">
           <IonFabButton>
-            <IonIcon icon={add}></IonIcon>
+            <IonIcon icon={document}></IonIcon>
           </IonFabButton>
-          <IonFabList side="top">
-            <IonFabButton>
-              <IonIcon icon={document}></IonIcon>
-            </IonFabButton>
-            <IonFabButton>
-              <IonIcon icon={colorPalette}></IonIcon>
-            </IonFabButton>
-            <IonFabButton>
-              <IonIcon icon={globe}></IonIcon>
-            </IonFabButton>
-          </IonFabList>
-        </IonFab>
+          <IonFabButton>
+            <IonIcon icon={colorPalette}></IonIcon>
+          </IonFabButton>
+          <IonFabButton>
+            <IonIcon icon={globe}></IonIcon>
+          </IonFabButton>
+        </IonFabList>
+      </IonFab>
     </IonContent>
   </>
 );
